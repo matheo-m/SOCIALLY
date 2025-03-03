4885254447e88c118b01ea3b38ad2251482ad1d8
MatheoM35 <matheomousse004@gmail.com>
2025-03-04 00:55:39 +0100

    refactor Home component; integrate CreatePost and PostCard components; fetch posts and user ID; add WhoToFollow section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
+import CreatePost from "@/components/CreatePost";
+import PostCard from "@/components/PostCard";
+// import PostCard from "@/components/PostCard";
+import WhoToFollow from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
+
   return (
-    <div className="m-4">
-      
-      <SignedOut>
-          <SignInButton mode="modal">
-            <Button>Sign in</Button>
-          </SignInButton>
-      <Button variant={"secondary"}>Sign in</Button>
-      </SignedOut>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
 
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
+      </div>
 
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
     </div>
   );
 }
